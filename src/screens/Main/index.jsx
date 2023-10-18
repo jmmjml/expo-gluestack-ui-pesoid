36edb5cc6c5a0ebf8d30dfe00b8db7edd3dffc21
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-10-18 12:59:52 +0000

    primeira

---
+++ src/screens/Main/index.jsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@gluestack-ui/themed";
+
+export default function Main() {
+    return (
+        <Box bg="$backgroundDark100">
+            <Text>Projeto Gluestack</Text>
+        </Box>
+    )
+}
