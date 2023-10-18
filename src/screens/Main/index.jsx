74cd130d5bbf1ea6b2b731420e93025f3b688710
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-10-18 14:07:27 +0000

    fouth

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@gluestack-ui/themed";
 
 export default function Main(){
     return (
-        <Box bg="$primary100" p="$2" height={"$full"} width={"$full"}>
+        <Box bg="$primary100" height={"$full"} width={"$full"}>
             <Text alignSelf="center">Projeto Gluestack</Text>
         </Box>
     )
