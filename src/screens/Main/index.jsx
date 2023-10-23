e4df91cc665a8213d4a0fddd886b22c1169f8ad4
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-10-23 14:58:36 +0000

    dskj

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,9 +1,36 @@
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box, Button, ButtonText, Center, FormControl, FormControlLabel, FormControlLabelText, HStack, Heading, Input, InputField, Switch, Text } from "@gluestack-ui/themed";
 
 export default function Main(){
     return (
-        <Box bg="$primary100" height={"$full"} width={"$full"}>
-            <Text alignSelf="center">Projeto Gluestack</Text>
+        <Box bg="$primary100" p="$5"  h={300} w={300} borderRadius="$3xl">
+            <Center h={"$full"}>
+                <Heading>Peso Ideal</Heading>
+                <FormControl>
+                    <FormControlLabel>
+                        <FormControlLabelText>Peso</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input w={"$full"}>
+                        <InputField />
+                    </Input>
+                    <FormControlLabel>
+                        <FormControlLabelText>Altura</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input w={"$full"}>
+                        <InputField />
+                    </Input>
+                    <FormControlLabel>
+                        <FormControlLabelText>Sexo</FormControlLabelText>
+                    </FormControlLabel>
+                    <HStack width={"$full"} space="md" justifyContent="center" alignItems="center">
+                        <FormControlLabelText>Homem</FormControlLabelText>
+                        <Switch/>
+                        <FormControlLabelText>Mulher</FormControlLabelText>
+                    </HStack>
+                    <Button py='$5'>
+                        <ButtonText >Calcular</ButtonText>
+                    </Button>
+                </FormControl>
+            </Center>
         </Box>
     )
 }
